1ee102471c528e6b6066dd217dd8821e55d2a618
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-02-20 22:31:30 +0000

    partial html for nav and alert messages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,28 +17,25 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params.fetch(:id))
   end
 
+  
   def create
-    movie_params = params.require(:movie).permit(:title, :description, :image_url)
-    
     @movie = Movie.new(movie_params)
-
+    
     if @movie.valid?
       @movie.save
-
+      
       redirect_to movies_url, notice: "Movie created successfully."
     else
       render "new"
     end
   end
-
+  
   def edit
     @movie = Movie.find(params.fetch(:id))
   end
-
+  
   def update
     @movie = Movie.find(params.fetch(:id))
-
-    movie_params = params.require(:movie).permit(:title, :description, :image_url)
     
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
@@ -46,12 +43,22 @@ class MoviesController < ApplicationController
       render "edit"
     end
   end
-
+  
   def destroy
     @movie = Movie.find(params.fetch(:id))
-
+    
     @movie.destroy
-
+    
     redirect_to movies_url, notice: "Movie deleted successfully."
   end
+end
+
+
+
+
+
+private
+
+def movie_params
+  params.require(:movie).permit(:title, :description, :image_url)
 end
